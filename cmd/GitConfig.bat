179630ea61ee74695977bdd950b667ab6ae5b480
chengxuncc <chengxuncc@gmail.com>
2022-06-07 19:51:58 +0800

    set git eol to lf

--- cmd/GitConfig.bat
+++ cmd/GitConfig.bat
@@ -1,5 +1,6 @@
 git config --global init.defaultBranch main
 git config --global core.autocrlf false
+git config --global core.eol lf
 git config --global credit.helper store
 git config --global user.email "chengxuncc@gmail.com"
 git config --global user.name "chengxuncc"
